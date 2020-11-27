f1ce3601ed031c396a61e30c89126ac9049ca7b1
Age Manning <Age@AgeManning.com>
2020-11-27 19:00:44 +1100

    Update to the latest discv5

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -1,9 +1,8 @@
 //! Handles the packet-based logic functions
 
 use clap::ArgMatches;
-use discv5::{enr, packet};
+use discv5::{enr, packet::Packet};
 use log::{error, info};
-use sha2::{Digest, Sha256};
 
 /// Decodes a packet based on the CLI options.
 pub fn decode(matches: &ArgMatches) {
@@ -23,13 +22,7 @@ pub fn decode(matches: &ArgMatches) {
 
     info!("Using decoding node id: {}", node_id);
 
-    let mut hasher = Sha256::new();
-    hasher.input(node_id.raw());
-    hasher.input(b"WHOAREYOU");
-    let mut magic: packet::Magic = Default::default();
-    magic.copy_from_slice(&hasher.result());
-
-    match discv5::packet::Packet::decode(&packet_bytes, &magic) {
+    match Packet::decode(&node_id, &packet_bytes) {
         Ok(p) => info!("Packet decoded: {:?}", p),
         Err(e) => error!("Packet failed to be decoded. Error: {:?}", e),
     }
