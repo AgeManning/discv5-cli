69d1c27897ff1bbb9d3cc7890a6e5a69a3a774b1
Andreas Bigger <abigger87@gmail.com>
2022-12-05 15:51:27 -0800

    :memo: readme touchups

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,41 @@
 //! ## Usage
 //!
 //! ```bash
+//! discv5-cli 0.2.5
 //! Sigma Prime <contact@sigmaprime.io>
-//! Simple CLI tool for starting and debugging discv5 servers. This currently runs a discv5 server which regularly performs
-//! peer search queries
+//! Simple CLI tool for starting and debugging discv5 servers and packets. This currently runs a discv5 server which
+//! regularly performs peer search queries.
 //!
 //! USAGE:
-//!     discv5-cli [FLAGS] [OPTIONS] --listen-address <IP-ADDRESS> --listen-port <PORT>
+//!  discv5-cli [OPTIONS] [SUBCOMMAND]
 //!
 //! FLAGS:
-//!     -w                  The Enr IP address and port will be the same as the specified listening address and port.
-//!     -h, --help          Prints help information
-//!     -k, --static-key    Use a fixed static key (hard-coded). This is primarily for debugging.
-//!     -V, --version       Prints version information
+//!  -h, --help       Prints help information
+//!  -V, --version    Prints version information
 //!
 //! OPTIONS:
-//!     -e, --enr <BASE64-ENR>               A base64 ENR that this node will initially connect to.
-//!         --enr-address <IP-ADDRESS>       Specifies the IP address of the ENR record. Not specifying this results in an
-//!                                          ENR with no IP field, unless the -w switch is used.
-//!         --enr-port <PORT>                Specifies the UDP port of the ENR record. Not specifying this results in an ENR
-//!                                          with no UDP field, unless the -w switch is used.
-//!     -l, --listen-address <IP-ADDRESS>    Specifies the listening address of the server. [default: 127.0.0.1]
-//!     -p, --listen-port <PORT>             Specifies the listening UDP port of the server. [default: 9000]
+//!     -v, --log-level <level>    Specifies the listening address of the server. [default: info]  [possible values: trace,
+//!                                debug, info, warn, error]
+//!
+//! SUBCOMMANDS:
+//!     help           Prints this message or the help of the given subcommand(s)
+//!     packet         Performs various packet encoding/decoding functions
+//!     request-enr    Requests the ENR of a multiaddr
+//!     server         Runs a discv5 test server
 //! ```
 //!
 //! ## Example
 //!
 //! ```bash
-//! $ discv5-cli -l 127.0.0.1 -p 9001 -w -e -IS4QCs0BSKEvnX8om4rAAi7D2p2lwQ7LVpAeESY2ikm1b5dBOqJC7istWMVg06dy-I09C8NuZdodEFNxIiiolWwSWkBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPKY0yuDUmstAHYpMa2_oxVtw0RW_QAdpzBQA8yWM0xOIN1ZHCCIyg
-//! Node Id: 0x98b6..42f9
-//! Peer Id: QmQJbNYRuLJo87Q77ZAj246NfPtctKsBbWtnTvJDLrGcw1
-//! Base64 ENR: enr:-IS4QKKLuzCDjV4hjA2-77cO0Xjx5vLGODQKe_Fl6Qim8qHBMN4chtaLqv6Xz6BWv5hfVvn0d2G0dt94ZAG9OHska44BgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQLHT5xE1ZhCXfrOv3D66tPfDZxV57TaX0rky3uwRhyBe4N1ZHCCIyk
-//! ip: 127.0.0.1, udp port:9001
-//! Connecting to ENR. ip: Some(127.0.0.1), udp_port: Some(9000),  tcp_port: None
-//! Query Completed. Nodes found:
-//! PeerId: QmdFQ2frpcyD3DiaMcM6XP37X3xCAV81GoB3jRusAa7kxu
-//! Connected Peers: 1
-//! Searching for peers...
-//! Query Completed. Nodes found:
-//! PeerId: QmdFQ2frpcyD3DiaMcM6XP37X3xCAV81GoB3jRusAa7kxu
+//! $ discv5-cli server -l 127.0.0.1 -p 9001 -w -e -IS4QCs0BSKEvnX8om4rAAi7D2p2lwQ7LVpAeESY2ikm1b5dBOqJC7istWMVg06dy-I09C8NuZdodEFNxIiiolWwSWkBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPKY0yuDUmstAHYpMa2_oxVtw0RW_QAdpzBQA8yWM0xOIN1ZHCCIyg
+//!
+//! 2020-05-20 21:15:06,968 INFO  [discv5_cli::server] Node Id: 0x62e5..2ca7
+//! 2020-05-20 21:15:06,968 INFO  [discv5_cli::server] Base64 ENR: enr:-IS4QKzeZIMc6NtdIYGTQvo0Q2Aw0NicbQrW7zArKWU6iNebWMuBTjJnes8nBJ-wAua-W6XNatKBHrNhxFcaUSysVJwBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQIAN7xJ9ce7O7iw-dFrlOzj4sC1Y0Gvic5hb5Rxfs4Xt4N1ZHCCIyk
+//! 2020-05-20 21:15:06,968 INFO  [discv5_cli::server] ip: 127.0.0.1, udp port:9001
+//! 2020-05-20 21:15:06,969 INFO  [discv5_cli::server] Connecting to ENR. ip: Some(127.0.0.1), udp_port: Some(9000),  tcp_port: None
+//! 2020-05-20 21:15:06,971 INFO  [discv5_cli::server::query_server] Connected Peers: 0
+//! 2020-05-20 21:15:06,971 INFO  [discv5_cli::server::query_server] Searching for peers...
+//! 2020-05-20 21:15:10,973 INFO  [discv5_cli::server::query_server] Query Completed. No peers found.
 //! ```
 
 mod cli;
